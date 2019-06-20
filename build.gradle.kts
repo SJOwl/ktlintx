1e5a3cd6f57da6415df8d9f8e046f80d37a57454
sj <vorobeievgeniy@gmail.com>
2019-06-20 13:50:11 +0300

    ktlintx init

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions.jvmTarget = "1.8"
+plugins {
+    id("java-library")
+    id("kotlin")
+}
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(kotlin("gradle-plugin", Versions.kotlin))
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    }
+}
+repositories {
+    mavenCentral()
+    jcenter()
+}
+dependencies {
+    implementation("com.github.shyiko.ktlint:ktlint-core:${Versions.ktlint}")
+
+    testImplementation("com.github.shyiko.ktlint:ktlint-core:${Versions.ktlint}")
+    testImplementation("com.github.shyiko.ktlint:ktlint-test:${Versions.ktlint}")
+    testImplementation("junit:junit:${Versions.Test.junit}")
+    testImplementation("org.amshove.kluent:kluent:${Versions.Test.kluent}")
+    testImplementation("org.assertj:assertj-core:${Versions.Test.assertj}")
+}
